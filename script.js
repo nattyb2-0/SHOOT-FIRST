2fcf2dca5261613433d67b461c1b0e2cd321c8d6
Natty Davis <natty.davis79@gmail.com>
2016-10-07 13:37:35 -0400

    add a few features to make player move continuosly

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 /////test to see if my page linked
 console.log('hello');
 //set up jquery for it to work upon the page loading
-$( document ).ready(function() {
+$(document).ready(function() {
     console.log( "ready!" );
     CrossHairMove();
     movePlayer();
     countShots();
+    timer();
+    targetMove();
+
+
 
     //move();
 });
@@ -15,7 +19,7 @@ var $maxDistanceX = window.innerWidth;
     $maxSpeed = 50;
     $minSpeed = 1000;
     var $randomX = Math.floor(Math.random()*$maxDistanceX);
-    var $randomY = Math.floor(Math.random()*$maxDistanceY);
+    var $randomY = Math.floor(Math.random()*($maxDistanceY-200));
     var $randomSpeed = Math.floor(Math.random*($minSpeed));
 
 /*create global variables to track info needed for the game
@@ -23,7 +27,10 @@ like the amount of times shots fired, number of targets hit,
 percentage etc*/
 var $shotsFired = 0,
     $targetHit = 0,
-    $percentage = 0;
+    $percentage = 0,
+    $target1Speed = 1000,
+    $target2Speed= 1000;
+
 
 /*create function to track the number of shots fired. each time the
 user clicks on the mouse the variable should increase by 1
@@ -52,28 +59,46 @@ create an array to store all my image. then create
 a function taht changes the image display to hidden.
 attach an event listener to the image array , along with
 my function.*/
-var $img = $('.gameimg');
-function makeImagesDisappear() {
-
-    $(this).css('visibility' , 'hidden');
+var $img = $('#t23');
+var $img1 =$('#t24')
+function kill() {
+    console.log('click event');
+    $(this).css('visibility', 'hidden');
     $targetHit++;
-
     console.log("target hit" + $targetHit);
     return $targetHit;
+    if($targetHit ===2 && counting > 0) {
+      level2();
+      counting +=15;
+    }else if($targetHit==4 && counting > 0) {
+      level3();
+      counting+=30;
+    }else if ($targetHit>5 && counting > 0) {
+      alert('You Have Mastered the art of Shoot First!!!')
+    } else {
+      $('.scores').html('GAME OVER!!!!');
+    }
 }
-$img.click(makeImagesDisappear);
+
+ $img.click(kill);
+ $img1.click(kill);
+
+
 
 /* set the images visibilty to hidden so whe the game
 loads you dont see them. then create an interval that
 allows images to become visible again, and fade out
-again as well.
-$img.hide()
-setInterval(function(){
- $img.fadeIn(1000)
-  $img.fadeOut(1000)
-}, 200);*/
+again as well.*/
+//$img.hide()
+// setInterval(function(){
+//  $img.fadeIn(1000),
+//   $img.fadeOut(1000),
+//   $img1.fadeIn(1000),
+//   $img1.fadeOut(1000)
 
-$crosshair = $('#crosshair')
+// }, 2200);
+
+var $crosshair = $('#crosshair');
 
 
 /* create the movement of the crosshair.
@@ -93,50 +118,38 @@ $crosshair = $('#crosshair')
   }
 
 
-/*
-/*function move() {
-  $("img").animate({
-            left: '+=5px',
-            height: '+=100px',
-            width: '+=10px'
 
-          },300)
-  console.log(left);
-}
 
-setInterval(function(){
-  move();
-}, 300);*/
-
+var $start = 0;
+  var $top = 0;
 function movePlayer() {
 
-  var $player = $('#23');
-  var $player2 = $('#22');
-
-  var $start = 0;
-  var $top = 0;
-
+  var $player2 = $('#t24');
   setInterval(function() {
   if ($start < 1200) {
   $start += Math.floor(Math.random()*$randomX);
 
-  $player.css('left', $start +"px");
-  $player.css('top', $top + "px");
+  $player2.css('left', (($start-130)*.5));
+  $player2.css('top', (($top-100)*.2))
+  setInterval(function(){
+ $img.fadeIn(1000)
+  $img.fadeOut(1000)
+}, 300);
 
-}else {
-  $start = 0;
+  }else {
+    $start = 0;
 
 }
   if ($top <= 600) {
      $top += Math.floor(Math.random()*$randomY);
 
-    $player.css('top', $top + "px");
+
 
   }else {
     $top = 0;
 
   }
-  },700);
+  },500);
 }
 
 
@@ -146,6 +159,11 @@ loop through the array
 add a set interval function in the loop
 */
 var $background =[
+  'http://assets.vg247.com/current//2016/01/cll_of-duty_black_ops_3_gauntlet-600x305.jpg',
+ 'http://cdn.themis-media.com/media/global/images/library/deriv/1265/1265801.png',
+ 'http://www.notebookcheck.net/fileadmin/Notebooks/Sonstiges/Games/Black_Ops_3/mood8_1.jpg',
+ 'http://cdn.segmentnext.com/wp-content/uploads/2016/03/Black-Ops-3-zombies-map-from-DLC-2.jpg',
+ 'https://i.kinja-img.com/gawker-media/image/upload/apazu5gn1v4ulzyowf1o.gif',
  'images/bacground3.jpg',
  'images/background1.jpg',
  'images/background2.jpg',
@@ -158,12 +176,59 @@ function changeBackground(i) {
     var $count = $background[i];
     $('.game-body').css('background-image', ('url('+$count+')'));
 }
-
 var count = 0;
 setInterval(function(){
+
   changeBackground(count);
   count++;
   if(count>5) {
     count = 0;
+
   }
 },5000)
+
+
+var counting=10;
+
+var counter=setInterval(timer, 1000);
+
+function timer()
+{
+  counting--;
+$('.scores').html(counting);
+  if (counting <= 0)
+  {
+       $('.scores').html('GAME OVER!!!!');
+
+     clearInterval(counter);
+
+
+
+  }
+
+
+}
+
+function moveRight() {
+  $('#t23').animate({ left: "1200px"},1600);
+}
+function moveDown() {
+   $('#t23').animate({top: "350px"}, 1600);
+ }
+function MoveLeft() {
+ $('#t23').animate({left: "0px"}, 1600);
+}
+function moveUp() {
+  $('#t23').animate( {top: "0px"}, 1600);
+}
+
+function targetMove() {
+
+  moveRight();
+  moveDown();
+  MoveLeft();
+  moveUp();
+
+}
+
+setInterval(targetMove, 30)
